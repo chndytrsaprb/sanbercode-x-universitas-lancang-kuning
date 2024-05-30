0798243f370a1f9d4fef0f07c4f5bf5ccb67a15e
chndytrsaprb <cindytresia01@gmail.com>
2024-05-30 11:17:35 -0700

    tugas13: laravel migration

--- tugas11-laravel1/app/Http/Controllers/homecontroller.php
+++ tugas11-laravel1/app/Http/Controllers/homecontroller.php
@@ -9,15 +9,5 @@ class HomeController extends Controller
     public function index() {
         return view('home');
     }
-
-    public function register() {
-        return view('page.register');
-    }
-
-    public function send(Request $request) {
-        $Firstname = $request->input('fname');
-        $Lastname = $request->input('lname');
-
-        return view('page.welcome', ['Firstname' => $Firstname, 'Lastname' => $Lastname, ]);
-    }
+    
 }
